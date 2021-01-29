9e287c4f46161b26f4383177c13eef5bb2be67c6
HadMatter00 <74080104+HadMatter00@users.noreply.github.com>
2021-01-29 15:28:26 -0500

    Added Player and started on movement

---
+++ Player/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+var speed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	movement(delta)
+
+func movement(delta):
+	var dir : Vector2 = Vector2()
+	
+	if Input.is_action_pressed("Up"):
+		dir.y -= 1
+	if Input.is_action_pressed("Down"):
+		dir.y += 1
+	if Input.is_action_pressed("Left"):
+		dir.x -= 1
+	if Input.is_action_pressed("Right"):
+		dir.x += 1
+	
+	dir = dir.normalized()
+	
+	move_and_slide(dir * speed, Vector2.UP)
