f2364e9a48bfbd6c4e24b4a51ae3fba7e1f43546
HadMatter00 <74080104+HadMatter00@users.noreply.github.com>
2021-01-29 19:26:41 -0500

    Implemented shield and camera rescaling

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,14 +1,30 @@
 extends KinematicBody2D
 
-var speed = 100
+var speed = 0
+var max_speed = 100
 
-var weapon = "sword"
+var weapon = "shield"
 
-var side = "right"
+var health = 10
+
+var charging = false
+var charge_vec = Vector2()
+
+var speed_dampening_multiplier = 5
+var default_speed_dampening = 5
+
+var last_dir = Vector2()
+
+const target_width = 320
+const target_height = 180
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	rescale_camera()
+
+func rescale_camera():
+	$Camera2D.zoom.x = target_width / get_viewport().size.x
+	$Camera2D.zoom.y = target_height / get_viewport().size.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,25 +33,46 @@ func _process(delta):
 func movement(delta):
 	var mouse_pos = get_local_mouse_position()
 	
-	if mouse_pos.x < 0:
-		side = "left"
-	else:
-		side = "right"
-	
 	var dir : Vector2 = Vector2()
 	
-	if Input.is_action_pressed("Up"):
-		dir.y -= 1
-	if Input.is_action_pressed("Down"):
-		dir.y += 1
-	if Input.is_action_pressed("Left"):
-		dir.x -= 1
-	if Input.is_action_pressed("Right"):
-		dir.x += 1
-	
-	dir = dir.normalized()
+	if charging:
+		dir = charge_vec
+		speed = max_speed * 1.5
+		speed_dampening_multiplier = 4
+		
+		last_dir = dir
+	else:
+		if Input.is_action_pressed("Up"):
+			dir.y -= 1
+		if Input.is_action_pressed("Down"):
+			dir.y += 1
+		if Input.is_action_pressed("Left"):
+			dir.x -= 1
+		if Input.is_action_pressed("Right"):
+			dir.x += 1
+		
+		dir = dir.normalized()
+		
+		if dir.x != 0 or dir.y != 0:
+			speed_dampening_multiplier = default_speed_dampening
+			
+			speed = min(max_speed, speed + max_speed * speed_dampening_multiplier * delta)
+			
+			last_dir = dir
+		else:
+			speed = max(0, speed - max_speed * speed_dampening_multiplier * delta)
+			
+			dir = last_dir
 	
 	move_and_slide(dir * speed, Vector2.UP)
 	
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
+func change_weapon(new_weapon):
+	weapon = new_weapon
+
+func get_weapon_damage():
+	if weapon == "sword": return 2
+	if weapon == "shield": return 3
+	return 1
--- Player/Shield.gd
+++ Player/Shield.gd
@@ -1,48 +1,44 @@
 extends Sprite
 
-var enemies_in_range = []
-
-var cooldown = 0
-
 var active = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if get_parent().weapon == "sword":
+	if get_parent().weapon == "shield":
 		visible = true
 		active = true
-		
-		if cooldown > 0:
-			cooldown = max(cooldown - delta, 0)
-			
-			if cooldown == 0:
-				for c in get_parent().get_children():
-					if c.name == "weapon_area": c.queue_free()
 	else:
 		visible = false
 		active = false
 
 func _input(event):
 	if active:
-		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and \
-		 event.pressed == true and cooldown == 0:
+		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+			if event.pressed == true:
+				var mouse_pos = get_parent().get_local_mouse_position()
+				
+				get_parent().charging = true
+				get_parent().charge_vec = mouse_pos.normalized()
+				
 				var area = Area2D.new()
 				var col_shape = CollisionShape2D.new()
 				col_shape.shape = CircleShape2D.new()
-				col_shape.shape.radius = 9
+				col_shape.shape.radius = 10
 				area.name = "weapon_area"
 				area.add_to_group("player_damage")
 				
-				var mouse_pos = get_parent().get_local_mouse_position()
-				
-				area.position += mouse_pos.normalized() * 16
+				area.position += mouse_pos.normalized() * 8
 				
 				get_parent().add_child(area)
 				area.add_child(col_shape)
 				
-				cooldown = 0.5
-	
+			elif event.pressed == false:
+				get_parent().charging = false
+				
+				for c in get_parent().get_children():
+					if c.name == "weapon_area": c.queue_free()
