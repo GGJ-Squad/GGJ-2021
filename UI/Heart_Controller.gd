1524f752d143fca0a31834eff5cdc3845b7a9716
yimmit303 <timsteiner303@gmail.com>
2021-01-30 18:18:17 -0500

    Change the UI mechanics

--- UI/Heart_Controller.gd
+++ UI/Heart_Controller.gd
@@ -4,22 +4,9 @@ var cur_health = 16
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	yield(get_tree().create_timer(1.0), "timeout")
-	damage(6)
-	yield(get_tree().create_timer(2.0), "timeout")
-	damage(1)
-	yield(get_tree().create_timer(2.0), "timeout")
-	damage(1)
-	yield(get_tree().create_timer(2.0), "timeout")
-	heal(5)
+	pass
 
 func heal(in_heal):
-	var h_index = 0
-	for child in self.get_children():
-		if child is AnimatedSprite:
-			if child.frame == 4:
-				h_index += 1
-				continue
 	$Tween.interpolate_property(get_node("Heart_Overlay"), "modulate", Color(0,1,0,1), Color(0,1,0,0), 0.5, Tween.TRANS_LINEAR)
 	$Tween.start()
 	set_health(cur_health + in_heal)
@@ -39,13 +26,11 @@ func damage(in_damage):
 	$Heart_Shards.emitting = true
 
 func set_health(health):
-	var h_index = -1
 	for child in self.get_children():
 		if child is AnimatedSprite:
 			child.frame = 0
 	for child in self.get_children():
 		if child is AnimatedSprite:
-			h_index += 1
 			if health > 4:
 				health -= 4
 				child.frame = 4
