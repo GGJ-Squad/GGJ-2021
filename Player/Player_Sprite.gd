f6d75b23d96619cab8553fe72bf555248c0ab664
yimmit303 <timsteiner303@gmail.com>
2021-01-30 12:12:04 -0500

    Player Sprite scene has script

---
+++ Player/Player_Sprite.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func change_state(state):
+	pass
+	
+func make_attack(weapon):
+	pass
+
+func hurt():
+	pass
