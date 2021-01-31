074fe6bbbd67f169ef83a1162f3394cef354792f
Ps20ndean <55252079+Ps20ndean@users.noreply.github.com>
2021-01-31 00:06:20 -0500

    Added Slug

--- Enemies/Slug.gd
+++ Enemies/Slug.gd
@@ -5,32 +5,31 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var health = 100
-var weapon = "claws"
+var weapon = "trail"
 var actor = self
 var rotate
 var patrol_location: Vector2 = Vector2.ZERO
 var patrol_location_reached = false
 var actor_velocity: Vector2= Vector2.ZERO
-var speed = 80
+var speed = 40
 var path = []
 var state ="Wander"
 var wander_range = 200
 onready var wander_timer = $WanderTimer
-onready var raycast_timer = $RaycastTimer
-onready var raycast = $RayCast2D
+onready var cooldown_timer = $CooldownTimer
 onready var nav = get_parent().get_node("LevelNav")
 var player_last_seen
 var player_detected = false
 var body
 var damage = 1
+var last_distance = 0
+var cooldown = false
 
 
 onready var target = get_tree().get_nodes_in_group("Players")[0]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Ai/Alert/CollisionShape2D").shape.radius = 50
-	get_node("Ai/Attack Range/CollisionShape2D").shape.radius =19
 	randomize()
 	var random_x = rand_range(-wander_range,wander_range)
 	var random_y = rand_range(-wander_range,wander_range)
@@ -41,14 +40,19 @@ func _ready():
 func _process(delta):
 	if state == "Wander":
 		wander(delta)
-	elif state == "Alert":
-		alert(delta)
-	elif state == "Attack":
-		attack(delta)
-		
+		if not cooldown:
+			
+			if get_parent() != null:
+				var inst = load("Enemies/SnailTrail.tscn").instance()
+				cooldown_timer.start()
+				cooldown=true
+				inst.position = position
+				inst.position.y += 8
+				inst.start(target)
+				get_parent().add_child(inst)
+				
 	if patrol_location_reached == false:
 		move_along_path(speed*delta)
-#		print(state)
 	$Label.text = state
 func wander(delta):
 	if not patrol_location_reached:
@@ -56,40 +60,7 @@ func wander(delta):
 			patrol_location_reached = true
 			actor_velocity = Vector2.ZERO
 			wander_timer.start()
-		
 
-func alert(delta):
-	raycast.cast_to = target.global_position - actor.global_position+Vector2(0,8)
-	raycast.cast_to *= 1.5
-	raycast.force_raycast_update()
-	if raycast.get_collider() == target or player_detected:
-		player_detected = true
-		_update_navigation_path(actor.position, target.global_position)
-		patrol_location_reached = false
-	else:
-		raycast_timer.start()
-		state = "Wander"
-
-func attack(delta):
-		target.take_damage(damage)
-
-
-func _on_Ai_state_changed(state,body):
-	self.state = state
-	self.body = body
-	if state == "Wander":
-		randomize()
-		var random_x = rand_range(-wander_range,wander_range)
-		var random_y = rand_range(-wander_range,wander_range)
-		patrol_location = nav.get_closest_point(Vector2(random_x,random_y) + global_position)
-		_update_navigation_path(actor.position, patrol_location)
-		patrol_location_reached = false
-	elif state == "Look":
-		player_last_seen = body.global_position
-		_update_navigation_path(actor.position, player_last_seen)
-		patrol_location_reached = false
-		player_detected = false
-		
 func move_along_path(distance):
 	var last_point = actor.position
 	while path.size():
@@ -138,6 +109,5 @@ func _on_Hurtbox_body_entered(body):
 		target.apply_knockback(actor.position)
 
 
-func _on_RaycastTimer_timeout():
-	if state == "Wander":
-		state = "Alert"
+func _on_CooldownTimer_timeout():
+	cooldown=false
